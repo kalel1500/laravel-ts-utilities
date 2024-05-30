8f74d61552e8747c553f2066fb429b89ef665347
Adrián <acanals@externos.itnow.es>
2024-05-30 17:56:00 +0200

    Exportar todas las clases en index.ts (para compilarlas y utilizarlas fuera)

---
+++ src/index.ts
@@ -0,0 +1,39 @@
+import LDate from "./dates/LDate";
+import InvalidValueException from "./ddd/domain/exceptions/InvalidValueException";
+import EnumVo from "./ddd/domain/objects/value-objects/EnumVo";
+import IntVo from "./ddd/domain/objects/value-objects/IntVo";
+import StringVo from "./ddd/domain/objects/value-objects/StringVo";
+import {_const, Config, Const} from "./helpers/Config";
+import g from "./helpers/global";
+import CannotOpenModalException from "./modals/sweetalert/CannotOpenModalException";
+import CannotOpenModalWarning from "./modals/sweetalert/CannotOpenModalWarning";
+import SModal from "./modals/sweetalert/SModal";
+import Notify from "./notifications/Notify";
+import Route from "./routing/Route";
+import Url from "./routing/Url";
+import LStorage from "./storage/LStorage";
+import {Ttable} from "./tables/Ttable";
+import EchoService from "./websockets/EchoService";
+import Websocket from "./websockets/Websocket";
+
+export {
+    LDate,
+    InvalidValueException,
+    EnumVo,
+    IntVo,
+    StringVo,
+    _const,
+    Config,
+    Const,
+    g,
+    CannotOpenModalException,
+    CannotOpenModalWarning,
+    SModal,
+    Notify,
+    Route,
+    Url,
+    LStorage,
+    Ttable,
+    EchoService,
+    Websocket,
+}
