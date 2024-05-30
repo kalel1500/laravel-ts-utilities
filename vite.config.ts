cd2be33c51a5dd84ff62e7bd7dcba10fd09c7730
Adrián <acanals@externos.itnow.es>
2024-05-30 17:59:58 +0200

    Configuracion vite.config.ts para no compliar los paquetes externos (tabulator, select, modal...)

--- vite.config.ts
+++ vite.config.ts
@@ -7,11 +7,23 @@ export default defineConfig({
         lib: {
             entry: 'src/index.ts', // path.resolve(__dirname, 'src/index.ts'),
             name: 'LaravelTsUtilities',
-            fileName: (format) => `laravel-ts-utilities.${format}.js`
+            fileName: (format) => `laravel-ts-utilities.${format}.js`,
+            formats: ['es']
         },
         rollupOptions: {
             // Asegúrate de externalizar las dependencias que no quieres incluir en tu paquete
-            external: [],
+            external: [
+                '@fortawesome/fontawesome-free',
+                '@popperjs/core',
+                'bootstrap',
+                'laravel-echo',
+                'luxon',
+                'pusher-js',
+                'slim-select',
+                'sweetalert2',
+                'tabulator-tables',
+                'ziggy-js',
+            ],
             output: {
                 globals: {}
             }
