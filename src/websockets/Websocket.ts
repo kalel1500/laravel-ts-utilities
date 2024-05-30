4e2fb21e8e0891b76a91a30b1c4b4635c5774133
Adrián <acanals@externos.itnow.es>
2024-05-30 15:27:13 +0200

    Imports faltantes actualizados v2

--- src/websockets/Websocket.ts
+++ src/websockets/Websocket.ts
@@ -1,8 +1,8 @@
-import LStorage from "../storage/LStorage";
-import g from "../helpers/global";
+import LStorage from "storage/LStorage";
+import g from "helpers/global";
 import {route} from "ziggy-js";
-import EchoService from "./EchoService";
-import SModal from "../modals/sweetalert/SModal";
+import EchoService from "websockets/EchoService";
+import SModal from "modals/sweetalert/SModal";
 import {_const} from "helpers/Config";
 import {FetchBroadcastingResponse, FetchResponse, FetchResponseOrBroadcasting, ResponseEventFetch} from "index";
 
