be64613748bc9c4ea4a3907eb900f7786aa26daf
Adrián <acanals@externos.itnow.es>
2024-06-06 16:31:28 +0200

    Refactor: reemplazar "," por ";" en las interfaces

--- src/_types/index.ts
+++ src/_types/index.ts
@@ -11,40 +11,40 @@ declare global {
 
 // Define una interfaz para la configuración por defecto
 export interface DefaultConstants {
-    token: string | null,
-    lang: string,
-    langDouble: string,
-    defaultZoneName: string,
-    appIcon: string,
+    token: string | null;
+    lang: string;
+    langDouble: string;
+    defaultZoneName: string;
+    appIcon: string;
 
-    VITE_BROADCASTING_ENABLED: boolean,
-    VITE_REVERB_APP_KEY: string|null,
-    VITE_REVERB_HOST: string,
-    VITE_REVERB_PORT: number,
-    VITE_REVERB_SCHEME: string,
-    VITE_APP_NAME: string
-    VITE_APP_ENV: string
+    VITE_BROADCASTING_ENABLED: boolean;
+    VITE_REVERB_APP_KEY: string|null;
+    VITE_REVERB_HOST: string;
+    VITE_REVERB_PORT: number;
+    VITE_REVERB_SCHEME: string;
+    VITE_APP_NAME: string;
+    VITE_APP_ENV: string;
 }
 
 export interface DefaultTranslations extends Translation {
-    ok: string,
-    reload_page: string,
-    contact_pi_team: string,
-    unforeseen_error: string,
-    loading: string,
-    loading_dots: string,
-    search: string,
-    filter: string,
-    date: string,
-    no_results: string,
-    download_completed: string,
-    fetch_error_message: string,
-    some_class_does_not_meet_the_INTERFACE_interface_contract: string,
-    it_has_been_called_a_JS_component_that_does_not_match_the_current_page: string,
-    need_NUMBER_characters: string,
-    a_loader_was_expected_on_the_page: string,
-    check_if_the_loader_was_in_a_div: string,
-    you_do_not_have_permissions_to_edit: string
+    ok: string;
+    reload_page: string;
+    contact_pi_team: string;
+    unforeseen_error: string;
+    loading: string;
+    loading_dots: string;
+    search: string;
+    filter: string;
+    date: string;
+    no_results: string;
+    download_completed: string;
+    fetch_error_message: string;
+    some_class_does_not_meet_the_INTERFACE_interface_contract: string;
+    it_has_been_called_a_JS_component_that_does_not_match_the_current_page: string;
+    need_NUMBER_characters: string;
+    a_loader_was_expected_on_the_page: string;
+    check_if_the_loader_was_in_a_div: string;
+    you_do_not_have_permissions_to_edit: string;
 }
 
 export interface FetchParams {
@@ -58,59 +58,59 @@ export interface FetchParams {
 }
 
 export interface FetchParamsSimple {
-    url: string
-    type: string
+    url: string;
+    type: string;
 }
 
 export interface CatchParams {
-    error: any
-    message?: string
-    clearMessage?: boolean
-    reloadOnClose?: boolean
-    footer?: string
-    from?: string
+    error: any;
+    message?: string;
+    clearMessage?: boolean;
+    reloadOnClose?: boolean;
+    footer?: string;
+    from?: string;
 }
 
 export interface FetchResponse {
-    success: boolean
-    message: string
-    data?: Record<string, any> | string
-    detail?: string
+    success: boolean;
+    message: string;
+    data?: Record<string, any> | string;
+    detail?: string;
 }
 
 export interface ResponseEventFetch {
-    response: FetchResponse
+    response: FetchResponse;
 }
 
 export interface FetchBroadcastingResponse extends FetchResponse {
     data: {
         broadcasting: FetchResponse,
         [p: string]: any
-    }
+    };
 }
 
-export type FetchResponseOrBroadcasting = FetchResponse | FetchBroadcastingResponse
+export type FetchResponseOrBroadcasting = FetchResponse | FetchBroadcastingResponse;
 
-export type AvailableValidations = 'optional' | 'required' | `min:${number}` | `max:${number}` | 'color' | 'array' | 'number' | 'boolean' | `required_if:${'1' | '0'}`
+export type AvailableValidations = 'optional' | 'required' | `min:${number}` | `max:${number}` | 'color' | 'array' | 'number' | 'boolean' | `required_if:${'1' | '0'}`;
 
 export type ValidationRules = {
-    [P: string]: AvailableValidations[] | string | number | boolean | null | undefined
-}
+    [P: string]: AvailableValidations[] | string | number | boolean | null | undefined;
+};
 
 export type ValidationResponse = {
-    success: boolean,
-    messages: string[],
-    validated: Record<string, any>
-}
+    success: boolean;
+    messages: string[];
+    validated: Record<string, any>;
+};
 
 export type ValidationInternalData = {
-    success: boolean[],
-    messages: string[],
-    validated: Record<string, any>
-}
+    success: boolean[];
+    messages: string[];
+    validated: Record<string, any>;
+};
 
-export type SyncOrAsync<T> = T | Promise<T> | { toPromise: () => T }
+export type SyncOrAsync<T> = T | Promise<T> | { toPromise: () => T };
 
-export type NullHTMLButtonElement = HTMLButtonElement | null
+export type NullHTMLButtonElement = HTMLButtonElement | null;
 
-export type TableSettingEvents = Partial<EventCallBackMethods>
+export type TableSettingEvents = Partial<EventCallBackMethods>;
