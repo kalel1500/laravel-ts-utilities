29b605c0d2be45188a79b059015c707205257728
Adrián <acanals@externos.itnow.es>
2024-06-14 16:11:52 +0200

    Quitar variable .env VITE_ENV y añadir VITE_MINIFY y VITE_SOURCEMAP (para controlar la minificación y el sourcemap

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,17 @@ import {defineConfig, loadEnv} from 'vite';
 import dts from 'vite-plugin-dts';
 import path from 'path';
 
-type Mode = 'production' | 'develop'
+type Mode = 'production' | 'develop';
+type StrBoolean = 'true' | 'false';
 type Env = {
-    VITE_ENV: 'pre' | 'pro'
+    VITE_MINIFY: StrBoolean,
+    VITE_SOURCEMAP: StrBoolean,
 }
 
 export default ({ mode }: { mode: Mode}) => {
     const env = loadEnv(mode, process.cwd()) as Env;
-    const isProd = env.VITE_ENV === "pro";
+    const minify = env.VITE_MINIFY === "true";
+    const sourcemap = env.VITE_SOURCEMAP === "true";
 
     return defineConfig({
         plugins: [
@@ -44,8 +47,8 @@ export default ({ mode }: { mode: Mode}) => {
                     globals: {}
                 }
             },
-            minify: isProd,
-            sourcemap: !isProd
+            minify: minify,
+            sourcemap: sourcemap
         }
     });
 }
