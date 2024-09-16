c2263d98ef65cdebb7947f00ee43cea34bc4970a
Adrián <acanals@externos.itnow.es>
2024-09-16 17:54:52 +0200

    mejorar imports bootstrap según la documentación

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 // Import styles to compile
 import "./styles";
 
-// Added: Actual Bootstrap JavaScript dependency
-import "bootstrap";
-
-// Added: Popper.js dependency for popover support in Bootstrap
-import "@popperjs/core";
-
 // Package exports
 export * from "./core";
 export * from "./providers";
