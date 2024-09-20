9f1b093d8dc7e3bce732af201819af4a692f0244
Adrián <acanals@externos.itnow.es>
2024-09-20 16:26:47 +0200

    Lógica para obtener el código de la aplicación extraída a la función getAppCode() + variables de entorno (EnvVariables) marcadas como opcionales (undefined)

--- src/plugins/vite-plugin-laravel-ts-utils.ts
+++ src/plugins/vite-plugin-laravel-ts-utils.ts
@@ -3,13 +3,14 @@ import { resolve } from 'path';
 import { loadEnv, Plugin, UserConfig } from 'vite';
 
 type EnvVariables = {
-    VITE_APP_ENV: string;
-    VITE_APP_NAME: string;
-    VITE_APP_CODE: string;
-    VITE_CONF_MINIFY: string;
-    VITE_CONF_SOURCEMAP: string;
-    VITE_CONF_USE_APPCODE_IN_SOURCE_PATH: string;
-}
+    VITE_APP_ENV?: string;
+    VITE_APP_NAME?: string;
+    VITE_APP_CODE?: string;
+    VITE_CONF_MINIFY?: string;
+    VITE_CONF_SOURCEMAP?: string;
+    VITE_CONF_USE_APPCODE_IN_SOURCE_PATH?: string;
+};
+type OpString = string | undefined;
 
 const optionalDependencies = [
     'bootstrap',
@@ -24,25 +25,33 @@ const isDependencyInstalled = (dep: string) => {
     }
 };
 
+const getAppCode = (confAppCode: OpString, confAppName: OpString) => {
+    return confAppCode?.toLocaleLowerCase()
+        ?? confAppName
+            ?.toLowerCase()
+            .replace(/(?:^\w|[A-Z]|\b\w|\s+|[_-])/g, (match, index) => index === 0 ? match.toLowerCase() : match.toUpperCase())
+            .replace(/\s+|[_-]/g, '');
+};
+
 export function laravelTsUtilsPlugin(): Plugin {
     const externalDependencies = optionalDependencies.filter(dep => !isDependencyInstalled(dep));
 
     return {
         name: 'vite-plugin-laravel-ts-utils',
         config(config, { mode }): UserConfig {
-            const env: EnvVariables                    = loadEnv(mode, process.cwd()) as EnvVariables;
-            const confAppName                   = env.VITE_APP_NAME;
-            const confAppCode                   = env.VITE_APP_CODE;
+            const env                       = loadEnv(mode, process.cwd()) as EnvVariables;
+            const confAppName           = env.VITE_APP_NAME;
+            const confAppCode           = env.VITE_APP_CODE;
             const confAppMinify                 = env.VITE_CONF_MINIFY === 'true';
             const confAppSourcemap              = env.VITE_CONF_SOURCEMAP === 'true';
             const confUseAppCodeInSourcePath    = env.VITE_CONF_USE_APPCODE_IN_SOURCE_PATH === 'true'
             const modeIsProd                    = mode === 'production';
 
-            const appCode = confAppCode?.toLocaleLowerCase() ?? confAppName?.toLowerCase().replace(/(?:^\w|[A-Z]|\b\w|\s+|[_-])/g, (match, index) => index === 0 ? match.toLowerCase() : match.toUpperCase()).replace(/\s+|[_-]/g, '');
-            const base = (confUseAppCodeInSourcePath && modeIsProd) ? `/${appCode}/build` : undefined;
+            const appCode = getAppCode(confAppCode, confAppName);
+            const basePublicPath = (confUseAppCodeInSourcePath && modeIsProd) ? `/${appCode}/build` : undefined;
 
             return {
-                base: base,
+                base: basePublicPath,
                 build: {
                     minify: confAppMinify,
                     sourcemap: confAppSourcemap,
