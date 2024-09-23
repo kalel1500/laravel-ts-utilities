313c1acfc48c31365592e702fdeaf6ac0d5e4ccf
Adrián <acanals@externos.itnow.es>
2024-09-23 14:10:48 +0200

    Solucionado error al excluir el TypeScript interno de la compilación

--- vite.config.ts
+++ vite.config.ts
@@ -18,7 +18,7 @@ export default ({ mode }: { mode: string }) => {
         plugins: [
             dts({
                 include: ['src/app'], // Incluye los directorios src y types para la generación de tipos
-                exclude: ['src/app/_internal/**'],
+                exclude: ['src/app/core/_internal/**'],
                 outDir: 'dist/app/types', // Directorio de salida para los archivos .d.ts || path.resolve(__dirname, 'dist/types')
             }),
         ],
