cc777d1b5baa56ba210817b5657eda74653af394
Adrián <acanals@externos.itnow.es>
2024-10-15 16:45:49 +0200

    (refactor) ordenar features del "UtilitiesServiceProvider.ts"

--- src/app/providers/UtilitiesServiceProvider.ts
+++ src/app/providers/UtilitiesServiceProvider.ts
@@ -1,22 +1,11 @@
 import { g, LStorage } from '../core';
 import {Notify} from "../core";
 
-type Actions = "registerGlobalError" | "enableTooltips" | "enableNotifications"
+type Features = keyof typeof UtilitiesServiceProvider.actions;
 
 export class UtilitiesServiceProvider {
 
-    static #actions = {
-        registerGlobalError: () => {
-            window.onerror = (message, source, lineno, colno, error) => {
-                return g.handleGlobalError(error);
-            };
-        },
-        enableTooltips: () => {
-            g.startTooltips();
-        },
-        enableNotifications: () => {
-            Notify.checkAndRequestPermission();
-        },
+    static actions = {
         startStorageDay: () => {
             if (LStorage.isFirstConnectionInDay()) {
                 LStorage.setNowAsLastConnection();
@@ -32,11 +21,23 @@ export class UtilitiesServiceProvider {
             // console.log(now.minus({day:2}).toFormat(MyLuxon.formats.date_startDay));
             // console.log(now.minus({day:2}).toFormat(MyLuxon.formats.timestamp_seconds));*/
         },
+        enableTooltips: () => {
+            g.startTooltips();
+        },
+
+        registerGlobalError: () => {
+            window.onerror = (message, source, lineno, colno, error) => {
+                return g.handleGlobalError(error);
+            };
+        },
+        enableNotifications: () => {
+            Notify.checkAndRequestPermission();
+        },
     };
 
-    static features(actions: Actions[]) {
+    static features(actions: Features[]) {
         actions.forEach(item => {
-            const action = this.#actions[item];
+            const action = this.actions[item];
             action();
         });
     }
