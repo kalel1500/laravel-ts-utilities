e5b8ffb1c3799e14110ac35d4e725921562799d5
Adrián <acanals@externos.itnow.es>
2024-10-26 04:55:57 +0200

    (fix) stubs: varios cambios -> + en algún momento se cambió el nombre de los estilos de "style.css" a "styles.css" + eliminar imports no usados en "HomeController.ts" + simplificar ruta home y eliminar test, ya que en algún momento se eliminó el controlador

--- src/cli/stubs/resources/js/app/routes.ts
+++ src/cli/stubs/resources/js/app/routes.ts
@@ -1,8 +1,6 @@
 import { Route } from 'laravel-ts-utilities';
 import HomeController from '../src/home/infrastructure/HomeController';
-import TestController from '../src/shared/infrastructure/TestController';
 
 export function defineRoutes(): void {
-    Route.page('shared.testJs', [TestController, 'testJs']);
-    Route.page('home.example1', [HomeController, 'example1']);
+    Route.page('home.index', [HomeController, 'index']);
 }
--- src/cli/stubs/resources/js/src/home/infrastructure/HomeController.ts
+++ src/cli/stubs/resources/js/src/home/infrastructure/HomeController.ts
@@ -1,6 +1,3 @@
-import { _const } from '../../../app/constants';
-import {g} from "laravel-ts-utilities";
-
 export default class HomeController {
     index() {
         console.log('inicio');
