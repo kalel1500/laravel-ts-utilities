c53b7fbc81767a54c4aee4ff13a76982db5ed9da
Adrián <acanals@externos.itnow.es>
2024-10-31 21:54:40 +0100

    (refactor) stubs: identar "postcss.config.js" y "tailwind.config.ts" con 4 espacios

--- src/cli/stubs/tailwind.config.ts
+++ src/cli/stubs/tailwind.config.ts
@@ -2,15 +2,15 @@ import type { Config } from 'tailwindcss'
 import { laravelContent, laravelDefaultPlugins } from 'laravel-ts-utilities/dist/plugins';
 
 export default {
-  content: [
-    ...laravelContent
-  ],
-  theme: {
-    extend: {
-      screens: {}
+    content: [
+        ...laravelContent
+    ],
+    theme: {
+        extend: {
+            screens: {}
+        },
     },
-  },
-  plugins: [
-    ...laravelDefaultPlugins
-  ],
+    plugins: [
+        ...laravelDefaultPlugins
+    ],
 } satisfies Config
